1ccac2b9e6b6388763f0e332c6adfb5ebc7f5596
Bruno Soares Maciel <contatosbrunomaciel@gmail.com>
2025-07-06 12:00:25 +0000

    atualiza o texto da página principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Essa página contém uma mensagem para Heitor Marques Maciel</h1>
+    return <><h1>Oi Princesa!</h1><p>Se você está vendo essa página... dá um sorriso pra mim. :)</p><p>Ah! Quando o seu Ateliê estiver bombando, imagina quem é que vai fazer a página do seu e-comerce?!!!</p><h3>Te amo!</h3></>
 }
 
 export default Home;

4074e6b2e90a34cb0500ed55e30489a21917632f
Bruno Soares Maciel <contatosbrunomaciel@gmail.com>
2025-07-12 15:47:10 +0000

    adciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
 function Home() {
-    return <><h1>Oi Princesa!</h1><p>Se você está vendo essa página... dá um sorriso pra mim. :)</p><p>Ah! Quando o seu Ateliê estiver bombando, imagina quem é que vai fazer a página do seu e-comerce?!!!</p><h3>Te amo!</h3></>
+  return (
+    <>
+      <h1>Oi Princesa!</h1>
+      <p>Se você está vendo essa página... dá um sorriso pra mim. :)</p>
+      <p>
+        Ah! Quando o seu Ateliê estiver bombando, imagina quem é que vai fazer a
+        página do seu e-comerce?!!!
+      </p>
+      <h3>Te amo!</h3>
+    </>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;

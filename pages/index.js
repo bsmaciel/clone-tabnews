62f86c5d225fdeb02cdc212402bcba0a727643f5
Bruno Soares Maciel <contatosbrunomaciel@gmail.com>
2026-06-27 19:24:49 +0000

    Fazendo ajustes no código html

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ function Home() {
     <div>
       <h1>Oi, filhão!</h1>
       <p>
-        Tudo bem? Olha que legal. Esse conteúdo está no meu domínio. Sei que não
-        é grandes coisas, mas caminhei bastante para chegar até aqui.
+        Tudo bem? Olha que legal. <br></br>
+        Esse conteúdo está no meu domínio (brunomaciel.com.br)
+        <br></br>
+        Sei que não é grandes coisas, mas caminhei bastante para chegar até
+        aqui.
       </p>
       <p>Avise quando visualizar a página.</p>
       <h3>Te amo!</h3>
